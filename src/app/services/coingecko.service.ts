02adc7b61e501701f27071559e37571e47d54218
Julian Zapata <julianzapata@estudiante.mdp.utn.edu.ar>
2023-10-26 22:57:39 -0300

    fixed api key usage, added crypto table

--- src/app/services/coingecko.service.ts
+++ src/app/services/coingecko.service.ts
@@ -1,33 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoingeckoService {
-  private apiURL = "https://api.coingecko.com/api/v3"
+  private apiURL = "https://api.coingecko.com/api/v3";
+  private apiKey = 'CG-96rk9R1Rk8GNats2evanpBka ';
+
   constructor(private http: HttpClient) { }
 
   getCryptocurrencyList(): Observable<any[]> {
     const url = `${this.apiURL}/coins/markets`;
-    const params = {
-      vs_currency: 'usd',
-      per_page: '100',
-      page: '1' 
-    };
+    const headers = new HttpHeaders().set('x-cg-demo-api-key', this.apiKey);
+    const params = new HttpParams()
+      .set('vs_currency', 'usd')
+      .set('per_page', '100')
+      .set('page', '1');
 
-    return this.http.get(url, { params }).pipe(
+    return this.http.get(url, { headers, params }).pipe(
       map((data: any) => data)
     );
   }
 
   getCryptocurrencyDetails(cryptoId: string): Observable<any> {
     const url = `${this.apiURL}/coins/${cryptoId}`;
-    return this.http.get(url);
+    const headers = new HttpHeaders().set('x-cg-demo-api-key', this.apiKey);
+
+    return this.http.get(url, { headers });
   }
-
 }
-
