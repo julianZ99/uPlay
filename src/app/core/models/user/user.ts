2c388f5d5be7154349665683b2a759893f727755
alanm <alanmedina1995@gmail.com>
2023-11-06 21:43:31 -0300

    Clase usuario.

---
+++ src/app/core/models/user/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    name: string;
+    lastname: string;
+    username: string;
+    password: string;
+    utncoin: number | undefined;
+
+    constructor(name: string, lastname: string, email: string, password: string) {
+        this.name = name;
+        this.lastname = lastname;
+        this.username = email;
+        this.password = password;
+    }
+}
