551acb430496de89a6b926a3bb259ec0b583633f
alanm <alanmedina1995@gmail.com>
2023-11-15 01:00:40 -0300

    Mejora de comportamiento en le login

--- src/app/core/services/uplay/uplay.service.ts
+++ src/app/core/services/uplay/uplay.service.ts
@@ -2,9 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../../models/user/user';
-import { tap } from 'rxjs/operators';
-import { AuthStatusService } from '../auth-status/auth-status.service';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -12,45 +9,50 @@ export class UplayService {
 
   private apiUplayURL = 'http://localhost:8080/api';
 
-  constructor(private http: HttpClient, private authStatusService: AuthStatusService) { }
+  constructor(private http: HttpClient) { }
 
-  login(username: string, password: string): Observable<User> {
+  login(username: string, password: string): Promise<User> {
     const url = `${this.apiUplayURL}/users/login`;
 
-    return this.http.post<User>(url, { username, password }).pipe(
-      tap((user: User) => {
-        this.authStatusService.setAuthenticatedUser(user);//autentico el user
-        console.log('Login successful:', user);
-      })
-    );
+    return new Promise<User>((resolve, reject) => {
+      this.http.post<User>(url, { username, password }).subscribe(
+        (response) => {
+          return resolve(response);
+        },
+        (error) => {
+          console.error('Error: ', error);
+          reject(error);
+        }
+      );
+    });
   }
 
   registration(user: User): Promise<any> {
     const url = `${this.apiUplayURL}/users/register`;
-  
+
     return new Promise<any>((resolve, reject) => {
-        this.http.post(url, user).subscribe(
-            (data) => {
-                resolve(data);
-            },
-            (error) => {
-                console.error('Error: ', error);
-                reject(error);
-            }
-        );
+      this.http.post(url, user).subscribe(
+        (data) => {
+          resolve(data);
+        },
+        (error) => {
+          console.error('Error: ', error);
+          reject(error);
+        }
+      );
     });
-}
+  }
 
   getCoinBalance(userId: number): Observable<number> {
     const url = `${this.apiUplayURL}/users/coin-balance/${userId}`;
 
     return this.http.get<number>(url);
   }
-  
+
   updateCoinBalance(userId: number, newBalance: number): Observable<any> {
     const url = `${this.apiUplayURL}/users/update-coin-balance/${userId}`;
     const params = { newCoinBalance: newBalance.toString() };
-  
+
     return this.http.put(url, null, { params });
   }
 }
