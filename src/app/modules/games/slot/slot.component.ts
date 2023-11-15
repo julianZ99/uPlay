db358714f971cea4970e986ef8d75daae55255b1
Julian Zapata <julianzapata@estudiante.mdp.utn.edu.ar>
2023-11-14 21:25:20 -0300

    slots funcional

--- src/app/modules/games/slot/slot.component.ts
+++ src/app/modules/games/slot/slot.component.ts
@@ -4,60 +4,60 @@ import { Subscription } from 'rxjs';
 
 import { AuthStatusService } from 'src/app/core/services/auth-status/auth-status.service';
 import { UplayService } from 'src/app/core/services/uplay/uplay.service';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
-
+import { symbols } from './symbols';
 
 @Component({
   selector: 'app-slot',
   templateUrl: './slot.component.html',
   styleUrls: ['./slot.component.css'],
-  animations: [
-    trigger('cardSpinner', [
-      state('in', style({ transform: 'translateY(100%)' })),
-      state('out', style({ display: 'none', transform: 'translateY(-100%)' })),
-      transition('* => out', [
-        style({ transform: 'translateY(100%)' }),
-        animate('0.5s', style({ transform: 'translateY(-100%)' })),
-      ]),
-      transition('* => in', [
-        style({ transform: 'translateY(-100%)' }),
-        animate('0.5s', style({ transform: 'translateY(100%)' })),
-      ]),
-    ]),
-  ],
+
 })
 
 export class SlotComponent implements OnInit, OnDestroy {
   authenticatedUser: any | null;
   coinBalance!: number;
-  playCost: number = 100;
-  rewardCoins: number = 50;
+  netCredits:any;
+  betCredits: number = 0; // apuesta
   isWinner: boolean | null = null;
 
-  currentIndex = 0;
-  isEnded = false;
-  intervalInstance: any;
+  idApuesta: number = 0;
 
-  cards = [
-    { value: 0, state: 'out', color: '#ffffff', transform: null },
-    { value: 1, state: 'out', color: '#ffffff', transform: null },
-    { value: 2, state: 'out', color: '#ffffff', transform: null },
-    { value: 3, state: 'out', color: '#ffffff', transform: null },
-    { value: 4, state: 'out', color: '#ffffff', transform: null },
-    { value: 5, state: 'out', color: '#ffffff', transform: null },
-    { value: 5, state: 'out', color: '#ffffff', transform: null },
-    { value: 5, state: 'out', color: '#ffffff', transform: null },
-    { value: 6, state: 'out', color: '#ffffff', transform: null },
-    { value: 7, state: 'out', color: '#ffffff', transform: null },
-    { value: 8, state: 'out', color: '#ffffff', transform: null },
-    { value: 9, state: 'out', color: '#ffffff', transform: null },
-  ];
+  details:any;
+  win: any;
+  lost: any;
+
+  stopSpin:any; 
+
+  currentSym1!: symbols;//Symbol1 in the reel
+  currentSym2!: symbols;//Symbol2 in the reel
+  currentSym3!: symbols;//Symbol3 in the reel
+
+  symbol1:symbols = {
+    value: 7,
+    symbolLink: "/assets/slots/redseven.png"
+  };
+  symbol2:symbols = {
+    value: 2,
+    symbolLink: "/assets/slots/cherry.png"
+  };
+  symbol3:symbols = {
+    value: 3,
+    symbolLink: "/assets/slots/lemon.png"
+  };
+  symbol4:symbols = {
+    value: 4,
+    symbolLink: "/assets/slots/plum.png"
+  };
+  symbol5:symbols = {
+    value: 5,
+    symbolLink: "/assets/slots/watermelon.png"
+  };
+  symbol6:symbols = {
+    value: 6,
+    symbolLink: "/assets/slots/bell.png"
+  };
+
+  symbolReel: symbols[] = [this.symbol1, this.symbol2, this.symbol3, this.symbol4, this.symbol5, this.symbol6];
 
   private authSubscription: Subscription = new Subscription();
 
@@ -69,6 +69,10 @@ export class SlotComponent implements OnInit, OnDestroy {
 
 
   ngOnInit() {
+    this.currentSym1 = this.symbolReel[2];
+    this.currentSym2 = this.symbolReel[4];
+    this.currentSym3 = this.symbolReel[5]; 
+    console.log("slots init");
     this.authSubscription = this.authStatusService.getAuthenticatedUser().subscribe((user) => {
       this.authenticatedUser = user;
       if (user) {
@@ -84,71 +88,100 @@ export class SlotComponent implements OnInit, OnDestroy {
     this.authSubscription.unsubscribe();
   }
 
-  isLoggedIn(): boolean {
-    return this.authStatusService.isLoggedIn();
+  stopSpinning(){
+    clearInterval(this.stopSpin);
+    this.winOrLose();
+
+
   }
 
-  playRoulette(): void {
-    if (this.coinBalance >= this.playCost) {
-      this.isWinner = Math.random() < 0.5;
+  spinning(){
+    if(this.betCredits > 0){
+    //spinning
+    this.stopSpin = setInterval(()=>{this.spin()},10);
+    }else{
+      alert("Insufficent Credits!");
+    }  
+  }
 
-      if (this.isWinner) {
-        this.coinBalance += this.rewardCoins;
-      } else {
-        this.coinBalance -= this.playCost;
-      }
+  spin(){
+    let randomNum1 = Math.floor(Math.random() * (this.symbolReel.length-1));
+    let randomNum2 = Math.floor(Math.random() * (this.symbolReel.length-1));
+    let randomNum3 = Math.floor(Math.random() * (this.symbolReel.length-1));
 
-      // Trigger the animation
-      this.animateSpin();
+    this.currentSym1 = this.symbolReel[randomNum1];
+    this.currentSym2 = this.symbolReel[randomNum2];
+    this.currentSym3 = this.symbolReel[randomNum3];
+  }
+  
+  betCoin(){
+    if(this.coinBalance > 0){
+      this.betCredits++;
+      this.netCredits++;
+      this.coinBalance--;
+    }
+    console.log("Bet Credits" + this.betCredits);
+    console.log("Coins" + this.coinBalance);
+  }
 
-      // ActualizoDB
-      this.uplayService.updateCoinBalance(this.authenticatedUser.id, this.coinBalance).subscribe(
-        () => {
-          console.log('Coin balance updated successfully.');
-        },
-        (error) => {
-          console.error('Error updating coin balance:', error);
-        }
-      );
-    } else {
-      console.log('Insufficient coins to play.');
+  betMax(){
+    if (this.coinBalance > 2){
+      this.betCredits += this.coinBalance;
+      this.netCredits += this.coinBalance;
+      this.coinBalance = 0;
     }
   }
 
-  animateSpin() {
-    this.isEnded = false;
-    this.cards.forEach((card) => (card.state = 'out'));
-    this.currentIndex = 0;
-    this.cards[this.currentIndex].state = 'in';
-
-    this.intervalInstance = setInterval(() => {
-      this.currentIndex++;
-      if (this.currentIndex === this.cards.length) {
-        this.currentIndex = 0;
-      }
-      if (this.currentIndex !== 0) {
-        this.cards[this.currentIndex - 1].state = 'out';
-      } else {
-        this.cards[this.cards.length - 1].state = 'out';
-      }
-      this.cards[this.currentIndex].state = 'in';
-    }, 1000);
-
-    const itemIndex = 5;
-    // Math.floor(
-    // Math.random() * (this.cards.length * 5 - this.cards.length) +
-    //   this.cards.length
-    // );
-
-    console.log(itemIndex);
-    setTimeout(() => {
-      clearInterval(this.intervalInstance);
-      const randomCard = this.cards.filter((card) => card.state === 'in');
-      this.isEnded = true;
-      console.log(randomCard);
-    }, itemIndex * 1000);
+  reset(){
+    this.coinBalance += this.betCredits;
+    this.betCredits = 0;
+    console.log("Bet Credits" + this.betCredits);
   }
+
+
+  winOrLose(){
+    //boolean cy compara
+    let slot1_eq_slot2: boolean = (this.currentSym1 == this.currentSym2);
+    let slot2_eq_slot3: boolean = (this.currentSym2 == this.currentSym3);
+    let slot1_eq_slot3: boolean = (this.currentSym1 == this.currentSym3);
+    
+    if(slot1_eq_slot2 || slot2_eq_slot3 || slot1_eq_slot3){
+      //gana si 2 o mas son iguales, y multiplyca x el valor del slot
+      if(slot1_eq_slot2){
+        this.coinBalance += (this.currentSym1.value * this.betCredits);
+      }else if(slot1_eq_slot3){
+        this.coinBalance += (this.currentSym3.value * this.betCredits);
+      }else if(slot2_eq_slot3){
+        this.coinBalance += (this.currentSym2.value * this.betCredits);
+      }
+      this.win++;
+      //settea bet=0
+      this.betCredits = 0;
+      alert("You WON!");
+    }else{
+      this.lost++;
+      //settea bet=0
+      this.betCredits = 0;
+      alert("You have Lost!");
+    }
+    //carga db con coins
+    this.uplayService.updateCoinBalance(this.authenticatedUser.id, this.coinBalance).subscribe(
+      () => {
+        console.log('Coin balance updated successfully.');
+      },
+      (error) => {
+        console.error('Error updating coin balance:', error);
+      }
+    );
+    
+    let winAndLost ={
+      idApuesta: this.idApuesta,
+      matchLost: this.lost,
+      matchWins: this.win
+    }
+
+    console.log(winAndLost);  
+  }
+
+
 }
-
-
-
